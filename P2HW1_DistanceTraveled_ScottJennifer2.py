849fa80679fa4bb70d51f6dd6b040469415c2660
ScottJ3101 <36613852+ScottJ3101@users.noreply.github.com>
2018-02-18 23:36:06 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_ScottJennifer2.py
@@ -0,0 +1,34 @@
+# CTI-110 
+# P2HW1 - Distance Traveled 
+# Jennifer Scott
+# 02/18/2018
+
+# Get the project Distance Traveled
+total_distance= float(input('Enter the projected speed: '))
+
+# Calculate the speed as 70 mph
+speed = total_distance * 70 
+
+# Display the speed
+print('The speed is 70 MPH', format("= distance / time" ))
+
+# Get the project Distance Traveled
+total_distance = float(input('enter the project time: '))
+
+# Calculate the time as 6 hour
+time = total_distance * 6
+
+#Display the time
+print ('The time is 6 hour', format('= speed / distance'))
+
+# Calculate the time as 10 hour
+time = total_distance * 10
+
+#Display the time
+print ('The time is 10 hour', format('= speed / distance'))
+
+# Calculate the time as 15 hour
+time = total_distance * 15
+
+#Display the time
+print ('The time is 15 hour', format('= speed / distance'))
